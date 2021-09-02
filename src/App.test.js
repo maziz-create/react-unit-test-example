74e6bd3279c9984a163e25a3cc54e3ca8ceb1682
Mehmet Aziz Algüllü <maacode@hotmail.com>
2021-09-02 11:41:49 +0300

    Added some commits

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
+/*
+NOT: BURASI TESTTE HATA ÇIKARTIYOR. 1 HATA VARSA ORASI BURASI.
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App />); //App componentini render et.
+
+  //doma bak içinde learn react yazan bi link var mı
   const linkElement = screen.getByText(/learn react/i);
+
+  //bekle.. dökümanın içinde de bu link var mı ? burası true dönerse testi geçer.
   expect(linkElement).toBeInTheDocument();
 });
+*/
